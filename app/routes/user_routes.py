fcb524b625bb77ac899165c13d54c8d08cce86fd
Victor Candeira <vscandeira@gmail.com>
2025-12-01 00:54:58 -0300

    Implementacao de autenticacao no get allusers

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,20 +1,24 @@
 from flask import Blueprint, jsonify
 from app import db
 from app.models.user import User
+from app.routes.default import get_token_from_request, is_token_valid
 
 users = Blueprint("users", __name__)
 
 @users.route('/allusers', methods=['GET'])
 def all_users():
-    # SELECT * FROM users
-    users = User.query.all()
+    token = get_token_from_request()
+    if is_token_valid(token):
+        # SELECT * FROM users
+        users = User.query.all()
 
-    # transformar em JSON
-    result = []
-    for u in users:
-        result.append({
-            "id": u.id,
-            "username": u.username
-        })
-
-    return jsonify(result)
+        # transformar em JSON
+        result = []
+        for u in users:
+            result.append({
+                "id": u.id,
+                "username": u.username
+            })
+        return jsonify(result)
+    else:
+        return jsonify({"message": "Unauthorized"}), 401
